caf9a8bf247192a6ce4798c5abf059ba10983647
[florchavez99@gmail.com] <florchavez99@gmail.com>
2023-06-01 13:46:43 -0300

    Practico N0 finalizado

--- areaRectangulo.js
+++ areaRectangulo.js
@@ -1,4 +1,8 @@
-let num1 = 5; //en el caso b el valor seria 12.5
-let num2 = 3;//en el caso b el valor seria 5.3
-let area = num1 * num2;
-console.log(area);
+let num1 = 5; 
+let num2 = 3;
+let num3 = 12.5;
+let num4 = 5.3;
+let areaA = num1 * num2;
+let areaB = num3 * num4;
+console.log(areaA);
+console.log(areaB);
--- conversionGrados.js
+++ conversionGrados.js
@@ -1,3 +1,6 @@
-let tempF1= 50; //en el caso b el valor seria 75
-let conversionGradosCelcius = (tempF1 -32) *5/9;
-console.log(conversionGradosCelcius);
+let tempF1= 50;
+let tempF2= 75;
+let conversionGradosCelciusA = (tempF1 -32) *5/9;
+let conversionGradosCelciusB = (tempF2 -32) *5/9;
+console.log(conversionGradosCelciusA);
+console.log(conversionGradosCelciusB);

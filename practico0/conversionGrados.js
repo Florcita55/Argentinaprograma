eed0b0f73e43314e11dcc0a6416a0e9e32d89363
[florchavez99@gmail.com] <florchavez99@gmail.com>
2023-06-13 21:10:08 -0300

    Agrego practico0

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,6 @@
+let tempF1= 50;
+let tempF2= 75;
+let conversionGradosCelciusA = (tempF1 -32) *5/9;
+let conversionGradosCelciusB = (tempF2 -32) *5/9;
+console.log(conversionGradosCelciusA);
+console.log(conversionGradosCelciusB);
---
+++ practico0/promedio3.js
@@ -0,0 +1,15 @@
+let num1= 5;//en el caso b el valor seria -2 y en el caso c -1.2
+let num2  = 7;//en el caso b el valor seria -3 y en el caso c 5.3
+let num3 = 9;//en el caso b el valor seria -8 y en l caso c -3.3
+let num4 = -2;
+let num5 = -3;
+let num6 = -8;
+let num7 = -1.2;
+let num8 = 5.3;
+let num9 = -3.3;
+let promedioA = (num1 + num2 + num3)/3;
+let promedioB = (num4 + num5 + num6)/3;
+let promedioC = (num7 + num8 + num9)/3;
+console.log(promedioA);
+console.log(promedioB);
+console.log(promedioC);

32008dd3d72b8170b8b8eb77f6a38c2e8ecc2009
[florchavez99@gmail.com] <florchavez99@gmail.com>
2023-06-13 21:16:57 -0300

    Elimino archivo

--- conversionGrados.js
+++
@@ -1,6 +0,0 @@
-let tempF1= 50;
-let tempF2= 75;
-let conversionGradosCelciusA = (tempF1 -32) *5/9;
-let conversionGradosCelciusB = (tempF2 -32) *5/9;
-console.log(conversionGradosCelciusA);
-console.log(conversionGradosCelciusB);

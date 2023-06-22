64cb09dec54fef2854120f21c1794ad2057ae949
[florchavez99@gmail.com] <florchavez99@gmail.com>
2023-06-22 20:54:47 -0300

    Agrego practico3

---
+++ practico3/lanzamiento.js
@@ -0,0 +1,12 @@
+let info = [10,9,8,7,6,5,4,3,2,1,0];
+let i=10;
+while (i >=0) {
+    if (i === 10){
+        console.log("Cuenta regresiva 10")
+    } else if (i === 0) {
+        console.log("Lanzamiento!")
+    }else{
+        console.log(i )
+    }
+  i--;
+}
